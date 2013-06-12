d26cbfeeb085028021f40a1bd31c20a6b5769934
diegofk26 <diegofk26@gmail.com>
2013-06-12 03:33:30 +0000

    modificacion en la creacion de barcos antes se creaban en la misma posicion todos

--- src/juego/ManejadorDeBarcos.java
+++ src/juego/ManejadorDeBarcos.java
@@ -12,86 +12,91 @@ import barcos.Vector;
 
 public class ManejadorDeBarcos {
 
-    private final ArrayList<Barco> barcos;
+	private final ArrayList<Barco> barcos;
 
-    public ManejadorDeBarcos() {
-        barcos = new ArrayList<Barco>();
-        this.crearBarcosConDireccionYSentidoAleatorio();
-    }
+	public ManejadorDeBarcos() {
+		barcos = new ArrayList<Barco>();
+		this.crearBarcosConDireccionYSentidoAleatorio();
+	}
 
-    public void crearBarcosConDireccionYSentidoAleatorio() {
-        Vector posicion = new Vector(0, 0);
-        Vector orientacion = new Vector(0, 0);
-        Vector direccion = new Vector(0, 0);
-        Lancha lancha;
-        Destructor destructor;
-        Buque buque;
-        Portaaviones portaaviones;
-        Rompehielo rompehielo;
+	public void crearBarcosConDireccionYSentidoAleatorio() {
+		Lancha lancha;
+		Destructor destructor;
+		Buque buque;
+		Portaaviones portaaviones;
+		Rompehielo rompehielo;
 
-        for (int x = 1; x < 3; x++) {
-            this.crearParametrosAleatoriosDeBarco(orientacion, posicion, direccion);
-            lancha = new Lancha(orientacion, posicion, direccion);
-            barcos.add(lancha);
-            this.crearParametrosAleatoriosDeBarco(orientacion, posicion, direccion);
-            destructor = new Destructor(orientacion, posicion, direccion);
-            barcos.add(destructor);
-        }
+		for (int x = 1; x < 3; x++) {
+			lancha = new Lancha(this.crearpOrientacionAleatoria(),
+					this.crearpPosicionAleatoria(),
+					this.crearDireccionAleatoria());
+			barcos.add(lancha);
 
-        this.crearParametrosAleatoriosDeBarco(orientacion, posicion, direccion);
-        portaaviones = new Portaaviones(orientacion, posicion, direccion);
-        barcos.add(portaaviones);
+			destructor = new Destructor(this.crearpOrientacionAleatoria(),
+					this.crearpPosicionAleatoria(),
+					this.crearDireccionAleatoria());
+			barcos.add(destructor);
+		}
 
-        this.crearParametrosAleatoriosDeBarco(orientacion, posicion, direccion);
-        rompehielo = new Rompehielo(orientacion, posicion, direccion);
-        barcos.add(rompehielo);
+		portaaviones = new Portaaviones(this.crearpOrientacionAleatoria(),
+				this.crearpPosicionAleatoria(), this.crearDireccionAleatoria());
+		barcos.add(portaaviones);
 
-        this.crearParametrosAleatoriosDeBarco(orientacion, posicion, direccion);
-        buque = new Buque(orientacion, posicion, direccion);
-        barcos.add(buque);
+		rompehielo = new Rompehielo(this.crearpOrientacionAleatoria(),
+				this.crearpPosicionAleatoria(), this.crearDireccionAleatoria());
+		barcos.add(rompehielo);
 
-    }
+		buque = new Buque(this.crearpOrientacionAleatoria(),
+				this.crearpPosicionAleatoria(), this.crearDireccionAleatoria());
+		barcos.add(buque);
 
-    private void crearParametrosAleatoriosDeBarco(Vector orientacion, Vector posicion, Vector direccion) {
-        posicion.setX((int) (Math.random() * 5) + 3);
-        posicion.setY((int) (Math.random() * 5) + 3);
-        orientacion.setX((int) (Math.random() * 2));
-        orientacion.setY(1 - orientacion.x());
-        direccion.setX((int) (Math.random() * 2));
-        direccion.setY((int) (Math.random() * 2));
-    }
+	}
 
-    public int cantidadDeBarcos() {
-        return barcos.size();
-    }
+	private Vector crearpPosicionAleatoria() {
+		return new Vector((int) (Math.random() * 5) + 1,
+				(int) (Math.random() * 5) + 1);
+	}
 
-    public int cantidadDeBarcosDestruidos() {
-        int contador = 0;
-        for (int x = 0; x < barcos.size(); x++) {
-            if ((barcos.get(x)).estaDestruido()) {
-                contador++;
-            }
-        }
-        return contador;
+	private Vector crearpOrientacionAleatoria() {
+		int x = (int) (Math.random() * 2);
+		return new Vector(x, 1 - x);
+	}
 
-    }
+	private Vector crearDireccionAleatoria() {
+		return new Vector((int) (Math.random() * 2), (int) (Math.random() * 2));
+	}
 
-    public void colocarBarcos() {
+	public int cantidadDeBarcos() {
+		return barcos.size();
+	}
 
-    }
+	public int cantidadDeBarcosDestruidos() {
+		int contador = 0;
+		for (int x = 0; x < barcos.size(); x++) {
+			if ((barcos.get(x)).estaDestruido()) {
+				contador++;
+			}
+		}
+		return contador;
 
-    public void moverBarcos() {
-        for (int x = 0; x < barcos.size(); x++) {
-            (barcos.get(x)).moverse();
-        }
+	}
 
-    }
+	public void colocarBarcos() {
 
-    public ArrayList<Barco> obtenerBarcos() {
-        return barcos;
-    }
+	}
 
-    public boolean todosLosBarcosEstanDestruidos() {
-        return (this.cantidadDeBarcosDestruidos()) == (this.cantidadDeBarcos());
-    }
+	public void moverBarcos() {
+		for (int x = 0; x < barcos.size(); x++) {
+			(barcos.get(x)).moverse();
+		}
+
+	}
+
+	public ArrayList<Barco> obtenerBarcos() {
+		return barcos;
+	}
+
+	public boolean todosLosBarcosEstanDestruidos() {
+		return (this.cantidadDeBarcosDestruidos()) == (this.cantidadDeBarcos());
+	}
 }

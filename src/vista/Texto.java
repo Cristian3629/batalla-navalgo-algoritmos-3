bf2250d020cd1869f1e2c39eba14074f7d539983
cristian3629 <cristian3629@gmail.com>
2013-07-03 23:09:17 +0000

    agregando ventana ayuda para los barcos y la clase Texto

---
+++ src/vista/Texto.java
@@ -0,0 +1,16 @@
+package vista;
+
+import java.awt.Color;
+
+import javax.swing.JLabel;
+
+public class Texto extends JLabel {
+    private final int tamX = 200;
+    private final int tamY = 30;
+
+    public Texto(String texto, int posX, int posY) {
+        super(texto);
+        this.setForeground(Color.BLACK);
+        this.setBounds(posX, posY, tamX, tamY);
+    }
+}

61e1d4121e6c27850d0c7308e1298c8f9544b785
t.arjovsky <t.arjovsky@gmail.com>
2013-07-04 15:51:08 +0000

    subo dos cambios porque un test de partida estaba choto y no me dejaba hacer el build. Hice que el de ver si ganó sea un test con persistencia.

--- src/partida/PartidaTest.java
+++ src/partida/PartidaTest.java
@@ -1,112 +1,111 @@
 package partida;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
-import java.util.ArrayList;
 
+import org.dom4j.Element;
 import org.junit.Test;
 
-import partes.Parte;
-import barcos.Barco;
 import barcos.Vector;
+import disparos.DisparoConvencional;
 
 public class PartidaTest {
 
-	@Test
-	public void testAlCrearUnaPartidaNuevaLosPuntosDebenSerMayoresACero() {
-		Partida partida = new Partida();
-		assertTrue(partida.getPuntos() > 0);
+    @Test
+    public void testAlCrearUnaPartidaNuevaLosPuntosDebenSerMayoresACero() {
+        Partida partida = new Partida();
+        assertTrue(partida.getPuntos() > 0);
 
-	}
+    }
 
-	@Test
-	public void testAlPasarUnTurnoLosPuntosDebenDisminuir() {
-		Partida partida = new Partida();
-		int puntosIniciales = partida.getPuntos();
-		partida.pasarTurno();
-		assertTrue(puntosIniciales > partida.getPuntos());
-	}
+    @Test
+    public void testAlPasarUnTurnoLosPuntosDebenDisminuir() {
+        Partida partida = new Partida();
+        int puntosIniciales = partida.getPuntos();
+        partida.pasarTurno();
+        assertTrue(puntosIniciales > partida.getPuntos());
+    }
 
-	@Test
-	public void testAlTerminarseLosPuntosElMetodoFaltanPuntosParaSeguirDeberiaDarTrue() {
-		Partida partida = new Partida();
-		for (int x = 1; x <= 50; x++) {
-			partida.colocarDaniador(partida.crearDaniador(
-					"disparoconvencional", new Vector(3, 3)));
-		}
-		assertTrue(partida.faltanPuntosParaSeguir());
-	}
+    @Test
+    public void testAlTerminarseLosPuntosElMetodoFaltanPuntosParaSeguirDeberiaDarTrue() {
+        Partida partida = new Partida();
+        for (int x = 1; x <= 50; x++) {
+            partida.colocarDaniador(partida.crearDaniador("disparoconvencional", new Vector(3, 3)));
+        }
+        assertTrue(partida.faltanPuntosParaSeguir());
+    }
 
-	@Test
-	public void testAlColocarUnaMinaDeberianDisminuirLosPuntos() {
-		Partida partida = new Partida();
-		Vector posicion = new Vector(10, 10);
-		int puntosIniciales = partida.getPuntos();
-		partida.colocarDaniador(partida.crearDaniador("minacontacto", posicion));
-		assertTrue(puntosIniciales > partida.getPuntos());
+    @Test
+    public void testAlColocarUnaMinaDeberianDisminuirLosPuntos() {
+        Partida partida = new Partida();
+        Vector posicion = new Vector(10, 10);
+        int puntosIniciales = partida.getPuntos();
+        partida.colocarDaniador(partida.crearDaniador("minacontacto", posicion));
+        assertTrue(puntosIniciales > partida.getPuntos());
 
-	}
+    }
 
-	@Test
-	public void testAlTratarDecolocarDaniadorsEnPosicionesInvalidasNoDeberianDecrecerLosPuntos() {
-		Partida partida = new Partida();
-		int puntosIniciales = partida.getPuntos();
-		for (int i = 0; i < 10; i++)
-			partida.colocarDaniador(partida.crearDaniador("minacontacto",
-					new Vector(101, 1100)));
-		assertEquals(puntosIniciales, partida.getPuntos());
-	}
+    @Test
+    public void testAlTratarDecolocarDaniadorsEnPosicionesInvalidasNoDeberianDecrecerLosPuntos() {
+        Partida partida = new Partida();
+        int puntosIniciales = partida.getPuntos();
+        for (int i = 0; i < 10; i++)
+            partida.colocarDaniador(partida.crearDaniador("minacontacto", new Vector(101, 1100)));
+        assertEquals(puntosIniciales, partida.getPuntos());
+    }
 
-	@Test
-	public void testGanarUnaPartida() {
-		Partida partida = new Partida();
-		ArrayList<Barco> barcos = partida.crearBarcosPorDefault();
-		ArrayList<Parte> partes;
-		Vector posicionDisparo;
-		for (int h = 1; h <= 2; h++) {
-			for (int i = 0; i < barcos.size(); i++) {
-				partes = (barcos.get(i)).obtenerPartes();
-				for (int j = 0; j < partes.size(); j++) {
-					posicionDisparo = (partes.get(j)).obtenerPosicion();
-					partida.colocarDaniador(partida.crearDaniador(
-							"disparoconvencional", posicionDisparo));
-				}
-			}
-		}
-		assertTrue(partida.gano());
-	}
+    @Test
+    public void testGanarUnaPartida() {
+        Partida partida;
+        try {
+            Element nodoPartida = Partida.obtenerNodoPartida("Niveles XML/nivelLanchaInmovil.xml");
+            partida = new Partida(nodoPartida);
+            partida.crearBarcos(nodoPartida);
+        } catch (IOException e) {
+            partida = new Partida();
+        }
 
-	@Test
-	public void testLevantaNivelSinError() throws IOException {
-		Partida partida = new Partida(
-				Partida.obtenerNodoPartida("niveles XML/NivelLanchaInmovil.xml"));
-	}
+        assertFalse(partida.gano());
+        DisparoConvencional disparo = new DisparoConvencional();
+        DisparoConvencional disparo2 = new DisparoConvencional();
+        disparo.cambiarCasillerosAfectados(new Vector(5, 5));
+        disparo2.cambiarCasillerosAfectados(new Vector(6, 5));
+        partida.colocarDaniador(disparo);
+        partida.colocarDaniador(disparo2);
+        partida.pasarTurno();
 
-	@Test
-	public void testPuntosInicialesDelNivel() {
-		Partida partida;
-		try {
-			partida = new Partida(
-					Partida.obtenerNodoPartida("niveles XML/NivelLanchaInmovil.xml"));
-		} catch (IOException e) {
-			partida = new Partida();
-		}
-		assertEquals(partida.getPuntos(), 2000);
-	}
+        assertTrue(partida.gano());
+    }
 
-	@Test
-	public void testLanchaEnPosicionCorrecta() {
-		Partida partida;
-		try {
-			partida = new Partida(
-					Partida.obtenerNodoPartida("niveles XML/NivelLanchaInmovil.xml"));
-		} catch (IOException e) {
-			partida = new Partida();
-		}
-		assertEquals(partida.getPuntos(), 2000);
+    @Test
+    public void testLevantaNivelSinError() throws IOException {
+        Partida partida = new Partida(Partida.obtenerNodoPartida("niveles XML/NivelLanchaInmovil.xml"));
+    }
 
-	}
+    @Test
+    public void testPuntosInicialesDelNivel() {
+        Partida partida;
+        try {
+            partida = new Partida(Partida.obtenerNodoPartida("niveles XML/NivelLanchaInmovil.xml"));
+        } catch (IOException e) {
+            partida = new Partida();
+        }
+        assertEquals(partida.getPuntos(), 2000);
+    }
+
+    @Test
+    public void testLanchaEnPosicionCorrecta() {
+        Partida partida;
+        try {
+            partida = new Partida(Partida.obtenerNodoPartida("niveles XML/NivelLanchaInmovil.xml"));
+        } catch (IOException e) {
+            partida = new Partida();
+        }
+        assertEquals(partida.getPuntos(), 2000);
+
+    }
 
 }

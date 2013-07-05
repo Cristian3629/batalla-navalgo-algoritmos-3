2f3640ca9e77a542ceafe39c560e5c3a9ee0d461
cristian3629 <cristian3629@gmail.com>
2013-07-05 05:05:37 +0000

    actualizando ventanaJuego y ventanaFondo

--- src/vista/VentanaFondo.java
+++ src/vista/VentanaFondo.java
@@ -1,16 +1,63 @@
 package vista;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class VentanaFondo extends VentanaGeneral {
+    ManejadorVentanas manejador;
+    String carpeta;
+    int posY = 350;
 
-    public VentanaFondo(String titulo, String dirImage) {
+    public VentanaFondo(ManejadorVentanas manejadorV, String titulo, String dirCarpeta) {
         super(titulo);
-        this.addImagenDeFondo(dirImage);
+        carpeta = dirCarpeta;
+        this.addBtnSalir();
+        this.addBtnVolverAjugar();
+        this.addBtnVolverInicio();
+        this.addImagenDeFondo(dirCarpeta + "/fondo.png");
+        manejador = manejadorV;
     }
 
-    /*
-     * private void addImagenDeFondo(String direccion) { ImageIcon iconoNuevaPartida = new
-     * ImageIcon(direccion); JLabel icono = new JLabel(iconoNuevaPartida); icono.setBounds(10, 40,
-     * 50, 100); this.getContentPane().add(icono); }
-     */
+    private void addBtnVolverAjugar() {
+        Boton btnVolverAJugar = new Boton(carpeta + "/volver.png", 292, posY);
+        btnVolverAJugar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                manejador.abrirTutorial(frame);
+            }
+        });
+        this.getContentPane().add(btnVolverAJugar);
+        // return btnVolverAJugar;
+
+    }
+
+    private void addBtnVolverInicio() {
+        String direccion = carpeta + "/inicio.png";
+        if (direccion.equals("imagenes/victoria/inicio.png")) {
+            System.out.println("la direccion esta bien");
+        }
+        Boton btnVolverInicio = new Boton(carpeta + "/inicio.png", 41, posY);
+        btnVolverInicio.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                manejador.abrirInicio(frame);
+            }
+        });
+        this.getContentPane().add(btnVolverInicio);
+        // return btnTutorial;
+
+    }
+
+    private void addBtnSalir() {
+        Boton btnSalir = new Boton(carpeta + "/salir.png", 543, posY);
+        btnSalir.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                frame.dispose();
+            }
+        });
+        this.getContentPane().add(btnSalir);
+        // return btnTutorial;
+
+    }
 }
